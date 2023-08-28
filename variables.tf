282b78b132cc670cdc26bc9e69176ce0d87a1d49
vimalshero <129414906+vimalshero@users.noreply.github.com>
2023-08-28 21:36:18 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "region" {
+  type =string
+  default = "ap-south-1"
+  
+}
+
+variable "subnet_id_1" {
+
+  type = string
+
+  default = "subnet-0f482614d61424d45"
+
+ }
+ variable "subnet_id_2" {
+
+  type = string
+
+  default = "subnet-04fcfc0feb57be751"
+
+ }
+ variable "subnet_id_3" {
+  
+  type = string
+
+  default = "subnet-04286909c4899a8f2"
+
+ }
+
+ variable "vpc_id" {
+  type = string
+  default = "vpc-06520588a9c196149"
+   
+ }
